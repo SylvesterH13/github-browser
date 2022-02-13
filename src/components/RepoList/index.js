0810f17754c3a58724386a4ae822a8f2e369d366
SylvesterH13 <henrique.sylvester@gmail.com>
2022-02-12 21:51:34 -0300

    Add logic to open repository page

--- src/components/RepoList/index.js
+++ src/components/RepoList/index.js
@@ -1,24 +1,34 @@
+import { useState } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function RepoList (props) {
+
+    const [ activeRepoId, setActiveRepoId ] = useState();
+
+    function GetListItem (repo) {
+        let createdAt = new Date(repo.created_at).toLocaleDateString('pt-BR');
+        return (
+            <ListGroup.Item
+                action key={repo.id}
+                active={repo.id == activeRepoId}
+                onClick={() => setActiveRepoId(repo.id)}
+                href={repo.html_url}
+                target="_blank"
+            >
+                <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">{repo.name}</h5>
+                    <small>Stars: {repo.stargazers_count}</small>
+                </div>
+                <p className="mb-1">{repo.description}</p>
+                <small>Created at {createdAt}</small>                    
+            </ListGroup.Item>
+        );
+    }    
+
     return (
         <ListGroup>
             {props.repos.map(GetListItem)}
         </ListGroup>        
-    );
-}
-
-function GetListItem (repo) {
-    let createdAt = new Date(repo.created_at).toLocaleDateString('pt-BR');
-    return (
-        <ListGroup.Item action key={repo.id}>
-            <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">{repo.name}</h5>
-                <small>Stars: {repo.stargazers_count}</small>
-            </div>
-            <p className="mb-1">{repo.description}</p>
-            <small>Created at {createdAt}</small>                    
-        </ListGroup.Item>
     );
 }
 
